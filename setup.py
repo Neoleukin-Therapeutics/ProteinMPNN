834d87197aa13409d5a976a6b5fd19f6fb496dbb
ShintaroMinami <sminami@neoleukin.com>
2022-06-07 18:06:55 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from codecs import open
+from os import path
+from setuptools_scm import get_version
+
+dir_path = path.abspath(path.dirname(__file__))
+
+with open(path.join(dir_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vanilla_proteinmpnn',
+    packages=['vanilla_proteinmpnn'],
+    license='MIT',
+    description='ProteinMPNN',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='@dauparas',
+    use_scm_version={'local_scheme': 'no-local-version'},
+
+    setup_requires=['setuptools_scm'],
+    install_requires=['numpy', 'torch'],
+ 
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.x',
+    ],
+)
---
+++ vanilla_proteinmpnn/__init__.py
@@ -0,0 +1,1 @@
+from .protein_mpnn_utils import ProteinMPNN, tied_featurize, parse_PDB, StructureDatasetPDB
