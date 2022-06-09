b153f4d23818c7f80cea694624af220e1be01c03
ShintaroMinami <sminami@neoleukin.com>
2022-06-09 04:48:49 -0700

    small fix

--- vanilla_proteinmpnn/__init__.py
+++ vanilla_proteinmpnn/__init__.py
@@ -1,1 +1,3 @@
+from os import path
 from .protein_mpnn_utils import ProteinMPNN, tied_featurize, parse_PDB, StructureDatasetPDB
+CHECKPOINT_DIRECTORY = path.join(path.dirname(path.realpath(__file__)), 'vanilla_model_weights/')
