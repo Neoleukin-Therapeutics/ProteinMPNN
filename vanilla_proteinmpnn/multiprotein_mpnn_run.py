eadd59fd7032828990293f2975c49a334021dd79
Kelly Duong <kellynduong@gmail.com>
2022-10-05 15:53:48 -0400

    save designs to fasta file

--- vanilla_proteinmpnn/multiprotein_mpnn_run.py
+++ vanilla_proteinmpnn/multiprotein_mpnn_run.py
@@ -230,7 +230,7 @@ def main(args):
                         
             # Generate some sequences
             name_ = "__".join([protein['name'] for protein in proteins])
-            ali_file = base_folder + '/seqs/' + name_ + '.fa'
+            ali_file = base_folder + '/seqs/' + name_ + '.fasta'
             score_file = base_folder + '/scores/' + name_ + '.npy'
             probs_file = base_folder + '/probs/' + name_ + '.npz'
             print(f'Generating sequences for: {name_}')
@@ -254,74 +254,11 @@ def main(args):
                             multi_bias_by_res=multi_bias_by_res_all,
                         )
                         S_sample = sample_dict["S"] 
-                        # else:
-                        #     sample_dict = model.tied_sample(X, randn_2, S, chain_M, chain_encoding_all, residue_idx, mask=mask, temperature=temp, omit_AAs_np=omit_AAs_np, bias_AAs_np=bias_AAs_np, chain_M_pos=chain_M_pos, omit_AA_mask=omit_AA_mask, pssm_coef=pssm_coef, pssm_bias=pssm_bias, pssm_multi=args.pssm_multi, pssm_log_odds_flag=bool(args.pssm_log_odds_flag), pssm_log_odds_mask=pssm_log_odds_mask, pssm_bias_flag=bool(args.pssm_bias_flag), tied_pos=tied_pos_list_of_lists_list[0], tied_beta=tied_beta, bias_by_res=bias_by_res_all)
-                        #     # Compute scores
-                        #     S_sample = sample_dict["S"]
-                        breakpoint()
-                        log_probs = model(X, S_sample, mask, chain_M*chain_M_pos, residue_idx, chain_encoding_all, randn_2, use_input_decoding_order=True, decoding_order=sample_dict["decoding_order"])
-                        mask_for_loss = mask*chain_M*chain_M_pos
-                        scores = _scores(S_sample, log_probs, mask_for_loss)
-                        scores = scores.cpu().data.numpy()
-                        all_probs_list.append(sample_dict["probs"].cpu().data.numpy())
-                        all_log_probs_list.append(log_probs.cpu().data.numpy())
-                        S_sample_list.append(S_sample.cpu().data.numpy())
                         for b_ix in range(BATCH_COPIES):
-                            masked_chain_length_list = masked_chain_length_list_list[b_ix]
-                            masked_list = masked_list_list[b_ix]
-                            seq_recovery_rate = torch.sum(torch.sum(torch.nn.functional.one_hot(S[b_ix], 21)*torch.nn.functional.one_hot(S_sample[b_ix], 21),axis=-1)*mask_for_loss[b_ix])/torch.sum(mask_for_loss[b_ix])
-                            seq = _S_to_seq(S_sample[b_ix], chain_M[b_ix])
-                            score = scores[b_ix]
-                            score_list.append(score)
-                            native_seq = _S_to_seq(S[b_ix], chain_M[b_ix])
-                            if b_ix == 0 and j==0 and temp==temperatures[0]:
-                                start = 0
-                                end = 0
-                                list_of_AAs = []
-                                for mask_l in masked_chain_length_list:
-                                    end += mask_l
-                                    list_of_AAs.append(native_seq[start:end])
-                                    start = end
-                                native_seq = "".join(list(np.array(list_of_AAs)[np.argsort(masked_list)]))
-                                l0 = 0
-                                for mc_length in list(np.array(masked_chain_length_list)[np.argsort(masked_list)])[:-1]:
-                                    l0 += mc_length
-                                    native_seq = native_seq[:l0] + '/' + native_seq[l0:]
-                                    l0 += 1
-                                sorted_masked_chain_letters = np.argsort(masked_list_list[0])
-                                print_masked_chains = [masked_list_list[0][i] for i in sorted_masked_chain_letters]
-                                sorted_visible_chain_letters = np.argsort(visible_list_list[0])
-                                print_visible_chains = [visible_list_list[0][i] for i in sorted_visible_chain_letters]
-                                script_dir = os.path.dirname(os.path.realpath(__file__))
-                                try:
-                                    commit_str = subprocess.check_output(f'git --git-dir {script_dir}/../.git rev-parse HEAD', shell=True).decode().strip()
-                                except subprocess.CalledProcessError:
-                                    commit_str = 'unknown'
-                                f.write('>{}, fixed_chains={}, designed_chains={}, model_name={}, git_hash={}\n{}\n'.format(name_, print_visible_chains, print_masked_chains, args.model_name, commit_str, native_seq)) #write the native sequence
-                            start = 0
-                            end = 0
-                            list_of_AAs = []
-                            for mask_l in masked_chain_length_list:
-                                end += mask_l
-                                list_of_AAs.append(seq[start:end])
-                                start = end
-
-                            seq = "".join(list(np.array(list_of_AAs)[np.argsort(masked_list)]))
-                            l0 = 0
-                            for mc_length in list(np.array(masked_chain_length_list)[np.argsort(masked_list)])[:-1]:
-                                l0 += mc_length
-                                seq = seq[:l0] + '/' + seq[l0:]
-                                l0 += 1
-                            score_print = np.format_float_positional(np.float32(score), unique=False, precision=4)
-                            seq_rec_print = np.format_float_positional(np.float32(seq_recovery_rate.detach().cpu().numpy()), unique=False, precision=4)
-                            f.write('>T={}, sample={}, score={}, seq_recovery={}\n{}\n'.format(temp,b_ix,score_print,seq_rec_print,seq)) #write generated sequence
-            if args.save_score:
-                np.save(score_file, np.array(score_list, np.float32))
-            if args.save_probs:
-                all_probs_concat = np.concatenate(all_probs_list)
-                all_log_probs_concat = np.concatenate(all_log_probs_list)
-                S_sample_concat = np.concatenate(S_sample_list)
-                np.savez(probs_file, probs=np.array(all_probs_concat, np.float32), log_probs=np.array(all_log_probs_concat, np.float32), S=np.array(S_sample_concat, np.int32), mask=mask_for_loss.cpu().data.numpy(), chain_order=chain_list_list)
+                            seq = _S_to_seq(S_sample[0][b_ix], multi_chain_M[0][b_ix])
+                            ix = j * BATCH_COPIES + b_ix
+                            f.write(
+                                f">{name_}__{ix}\n{seq}\n") #write generated sequence
             t1 = time.time()
             dt = round(float(t1-t0), 4)
             num_seqs = len(temperatures)*NUM_BATCHES*BATCH_COPIES
